c7c108b427d2c00f6dba2912dd22788a8cb03c65
杨小伟 <yangxiaowei@yangxiaoweideMacBook-Pro.local>
2019-09-05 23:56:04 +0800

    路由跳转

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { connect } from 'dva';
+// 引入路由需要组件
+import { Switch, Route, Redirect } from "dva/router";
+
 import styles from './IndexPage.scss';
 import  Navbar from './Navbar'
-import Home from './Home'
+import Home from './Home';
+import Admin from "./Admin";
+import About from "./About";
+import Menus from "./Menus";
+import Login from "./user/login";
+import Regist from "./user/regist";
 import { Layout } from 'antd'
 const {Header, Content} = Layout
 
@@ -13,7 +21,17 @@ function IndexPage() {
         <Navbar />
       </Header>
       <Content className={styles.content}>
-        <Home />
+        {/* 一极路由 */}
+        <Switch>
+          <Route path="/home" component= {Home} />
+          <Route path="/menus" component= {Menus} />
+          <Route path="/about" component= {About} />
+          <Route path="/admin" component= {Admin} />
+          <Route path="/login" component= {Login} />
+          <Route path="/regist" component= {Regist} />
+
+          <Redirect to='/home' />
+        </Switch>
       </Content>
     </Layout>
   );
--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Menu} from 'antd'
+import {Link} from 'dva/router'
 import styles from './index.scss'
 
 export default function index() {
@@ -7,12 +8,24 @@ export default function index() {
     <nav className={styles.nav}>
       <span className={styles.logo}></span>
       <Menu className={styles['menu-left']} mode="horizontal" defaultSelectedKeys={['home']}>
-        <Menu.Item key={'home'}>主页</Menu.Item>
-        <Menu.Item key={'menus'}>菜单</Menu.Item>
-        <Menu.Item key={'admin'}>管理</Menu.Item>
-        <Menu.Item key={'about'}>关于我们</Menu.Item>
-        <Menu.Item key={'login'} className={styles.login}>登录</Menu.Item>
-        <Menu.Item key={'regist'} className={styles.registe}>注册</Menu.Item>
+        <Menu.Item key={'home'}>
+          <Link to="/home">主页</Link>
+        </Menu.Item>
+        <Menu.Item key={'menus'}>
+        <Link to="/menus">菜单</Link>
+        </Menu.Item>
+        <Menu.Item key={'admin'}>
+        <Link to="/admin">管理</Link>
+        </Menu.Item>
+        <Menu.Item key={'about'}>
+        <Link to="/about">关于我们</Link>
+        </Menu.Item>
+        <Menu.Item key={'login'} className={styles.login}>
+        <Link to="/login">登录</Link>
+        </Menu.Item>
+        <Menu.Item key={'regist'} className={styles.registe}>
+        <Link to="/regist">注册</Link>
+        </Menu.Item>
       </Menu>
     </nav>
   )
