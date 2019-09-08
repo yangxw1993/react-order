365e2d32b07365cd9ff1ba967bc5a28cfca8fda3
杨小伟 <yangxiaowei@yangxiaoweideMacBook-Pro.local>
2019-09-08 14:46:44 +0800

    路由加载

--- src/router.js
+++ src/router.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Router, Switch } from 'dva/router';
 import IndexPage from './pages/IndexPage';
- import Home from "./pages/Home";
- import Menus from "./pages/Menus";
- import Admin from "./pages/Admin";
- import About from "./pages/About";
- import Login from "./pages/user/login";
- import Regist from "./pages/user/regist";
- import SubRoutes from './utils/SubRoutes'
+import SubRoutes from './utils/SubRoutes'
 
 const RouteConfig = [
   {
@@ -16,28 +10,28 @@ const RouteConfig = [
     routes: [
       {
         path: '/home',
-        component: Home, 
+        component: require('./pages/Home'), 
         redirect: true
       },
       {
         path: '/menus',
-        component: Menus,
+        component: require('./pages/Menus'),
       },
       {
         path: '/admin',
-        component: Admin,
+        component: require('./pages/Admin'),
       },
       {
         path: '/about',
-        component: About,
+        component: require('./pages/About'),
       },
       {
         path: '/login',
-        component: Login,
+        component: require('./pages/user/login'),
       },
       {
         path: '/regist',
-        component: Regist,
+        component: require('./pages/user/regist'),
       }
     ]
   }
