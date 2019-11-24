45e36b02c33a71a0188a52ce599dc45ea5cc1abe
杨小伟 <18813068824@163.com>
2019-11-24 23:44:27 +0800

    update: 渲染table

--- src/pages/Menus/index.js
+++ src/pages/Menus/index.js
@@ -1,10 +1,103 @@
 import React from 'react'
-
+import {Button, Icon, Table} from 'antd';
 export default function index() {
+  const columns = [
+    {
+      title: '商品',
+      dataIndex: 'name',
+      key: 'name',
+      // render: text => <a>{text}</a>,
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: '内存',
+      dataIndex: 'size',
+      key: 'size',
+      render: text => <span>{text}</span>
+    },
+    {
+      title: '加入',
+      key: 'action',
+      render: text => {
+        const obj = {
+          children: (
+            <Button>
+              <Icon type='plus' />
+            </Button>
+          )
+        };
+        return obj
+      }
+    }
+    /*{
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <span>
+        <a>Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a>Delete</a>
+      </span>
+      ),
+    },*/
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: 'MacBook Pro',
+      description: '好用的Mac',
+      options:[
+        {
+          size: 13,
+          price: 13000
+        },
+        {
+          size: 15,
+          price: 18000
+        }
+      ]
+    },
+    {
+      key:2,
+      name: 'iPhone 7',
+      description: '好用的iPhone',
+      options:[
+        {
+          size: 32,
+          price: 5000
+        },
+        {
+          size: 64,
+          price: 6000
+        }
+      ]
+    },
+    {
+      key: 3,
+      name: '小米 6',
+      description: '好用的小米',
+      options:[
+        {
+          size: 64,
+          price: 2800
+        },
+        {
+          size: 128,
+          price: 3600
+        }
+      ]
+    }
+  ];
   return (
-    <div>
-      menus
-    </div>
+    <Table
+      pagination={false}
+      columns={columns}
+      dataSource={data} />
   )
 }
 
